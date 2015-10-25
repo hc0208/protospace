6f5e53091f4f36f7509e9faf7d4637e521e65bdb
Hiroki Chida <hiroki22991395@yahoo.co.jp>
2015-10-25 15:24:06 +0900

    画像以外の投稿機能を実装しました。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,13 @@
 class PrototypesController < ApplicationController
   def new
   end
+  def create
+    Prototype.create(title: prototype_params[:title], catchcopy: prototype_params[:catchcopy], concept: prototype_params[:concept], user_id: current_user.id)
+    redirect_to controller: 'prototype/ranking', action: 'index'
+  end
+
+  private
+  def prototype_params
+    params.permit(:title, :catchcopy, :concept, :user_id)
+  end
 end
