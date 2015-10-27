34b97e4a66007fbbb853e936b19c65ecd3c7e35c
Hiroki Chida <hiroki22991395@yahoo.co.jp>
2015-10-27 14:02:32 +0900

    thumbnailsテーブルにprototype_idが保存されるようにしました。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,8 +2,8 @@ class PrototypesController < ApplicationController
   def new
   end
   def create
-    Prototype.create(title: prototype_params[:title], catchcopy: prototype_params[:catchcopy], concept: prototype_params[:concept], user_id: current_user.id)
-    Thumbnail.create(image: thumbnail_params[:image])
+    prototype = Prototype.create(title: prototype_params[:title], catchcopy: prototype_params[:catchcopy], concept: prototype_params[:concept], user_id: current_user.id)
+    Thumbnail.create(image: thumbnail_params[:image], prototype_id: prototype.id)
     redirect_to controller: 'prototype/ranking', action: 'index'
   end
 
