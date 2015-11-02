d3910a8c252df3b2728988bb2a7187a4a4ee7b6a
Hiroki Chida <hiroki22991395@yahoo.co.jp>
2015-11-02 22:39:47 +0900

    プロトタイプ削除機能を実装しました。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,13 +3,26 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.new
     @prototype.thumbnails.build
   end
+
   def create
     @prototype = Prototype.create(prototype_params)
     redirect_to controller: 'prototype/ranking', action: 'index'
   end
 
+  def destroy
+    prototype = Prototype.find(id_params[:id])
+    if prototype.user_id == current_user.id
+      prototype.destroy
+    end
+    redirect_to :root
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catchcopy, :concept, :user_id, thumbnails_attributes: [:image, :role]).merge(user_id: current_user.id)
+  end
+
+  def id_params
+    params.permit(:id)
   end
 end
