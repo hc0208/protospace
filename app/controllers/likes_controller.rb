03eafc8e5a45ea9c3282d5acdd768cbf222d1485
Hiroki Chida <hiroki22991395@yahoo.co.jp>
2015-11-10 17:05:12 +0900

    set_likeをprivate以下へ移動しました。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -9,13 +9,13 @@ class LikesController < ApplicationController
     set_like
   end
 
-  def set_like
-    @prototype = Prototype.find(params[:prototype_id])
-    @likes = Like.find_by(prototype_id: params[:prototype_id], user_id: current_user.id) if user_signed_in?
-  end
-
   private
   def like_params
     params.permit(:user_id, :prototype_id).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
+  def set_like
+    @prototype = Prototype.find(params[:prototype_id])
+    @likes = Like.find_by(prototype_id: params[:prototype_id], user_id: current_user.id) if user_signed_in?
   end
 end
