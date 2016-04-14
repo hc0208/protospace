f62040f03282fd03ffbdc08ef90b152223d5fe0e
Hiroki Chida <hiroki22991395@yahoo.co.jp>
2016-04-14 14:29:39 +0900

    Refactoring function comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(comment_params)
+    current_user.comments.create(comment_params)
     @prototype = Prototype.find(params[:prototype_id])
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:prototype_id, :user_id, :text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+    params.require(:comment).permit(:user_id, :text).merge(prototype_id: params[:prototype_id])
   end
 end
